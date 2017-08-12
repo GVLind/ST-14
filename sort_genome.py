b8d0a97b800906307cf97a6d43f5d4a1f6197534
GVLind <vctrlndh@gmail.com>
2017-08-12 23:41:23 +0200

    finished script as proof of concept.

--- sort_genome.py
+++ sort_genome.py
@@ -1,4 +1,44 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+
+def FormatNames (string):
+
+	tmpstring = string.replace(" ","")
+	tmpstring = tmpstring.replace("(","")
+	tmpstring = tmpstring.replace(")","")
+	tmpstring = tmpstring.replace("/","")
+	tmpstring = tmpstring.replace("[","")
+	tmpstring = tmpstring.replace("]","")
+	tmpstring = tmpstring.replace("+","")
+	tmpstring = tmpstring.replace("\'","")
+	tmpstring = tmpstring.replace("\"","")
+	tmpstring = tmpstring.replace("\n","")
+	outstring =tmpstring.replace(">","\n>")
+
+	return outstring
+
+def MakeProteinfamilyFiles (PFlist,header, Dbfile,inputpath):
+	import subprocess
+
+	try:
+		subprocess.call(["mkdir","%s/%s"%(inputpath,header)])
+
+		#maybe some cool exception??
+
+	print ("grepping amino acids for proteinfamilies belonging to %s"%(header))	
+	for i in PFlist:
+		print ("%s/%s"%(i[0],len(PFlist)), end="\r")
+
+		meta = i[1][0:4]
+		for j in i[2:]:
+			if j != "*":
+
+				p1 = subprocess.Popen(["grep","%s"%(j),"%s"%(Dbfile)],stdout=subprocess.PIPE)
+				proteinfamilyfile = open("%s/%s/%s%s"%(inputpath,header,i[0],meta),"a")
+				p2 = subprocess.Popen(["sed","s/###/\\n/g"],stdin = p1.stdout,stdout = proteinfamilyfile)
+				
+				proteinfamilyfile.close()
+				p1.stdout.close()
+	print ("%s files written to %s"%(len(PFlist),header))
 
 import argparse
 import os
@@ -7,6 +47,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-i", dest="Inputfolder", required=True, help="folder containging myproject.proteinortho and additional .faa used creating myproject.proteinortho")
 args = parser.parse_args()
 parser.parse_args()
+
 
 
 # compressing .faa files to a temporary database.
@@ -19,7 +60,6 @@ os.system(execstring)
 
 #formatting Db
 
-#Maybe some kind of buffer owerflow. hav to fix this
 tmpstring=""
 with open (Dbfile,'r') as fi:
 
@@ -27,16 +67,7 @@ with open (Dbfile,'r') as fi:
 		if line.startswith(">"):
 			line = line + "###"
 		tmpstring=tmpstring + line
-	tmpstring = tmpstring.replace(" ","")
-	tmpstring = tmpstring.replace("-","")
-	tmpstring = tmpstring.replace("(","")
-	tmpstring = tmpstring.replace(")","")
-	tmpstring = tmpstring.replace("/","")
-	tmpstring = tmpstring.replace("[","")
-	tmpstring = tmpstring.replace("]","")
-	tmpstring = tmpstring.replace("+","")
-	tmpstring = tmpstring.replace("\n","")
-	tmpstring =tmpstring.replace(">","\n>")
+	tmpstring = FormatNames(tmpstring)
 
 fi.close()
 
@@ -46,7 +77,7 @@ with open (Dbfile,"w+") as fo:
 	fo.write(tmpstring)
 fo.close
 
-# creating dictionary containing proteinfamilies
+# creating list containing proteinfamilies
 
 foldercontent = os.listdir(inputpath)
 proteinfamilies = []
@@ -61,51 +92,114 @@ for item in foldercontent:
 			for line in proteinorthoOut:
 				proteinfamilyWnumber=[]
 				splitline=line.split('\t')
-				proteinfamily=splitline[3:]
+				newsplitline=[]
 
-				proteinfamilycounter +=1
+
+
+				for i in splitline:
+					tmpstring = FormatNames(i)
+					newsplitline.append(tmpstring)
+
+
+				proteinfamily=newsplitline[3:]
+
+
+				
 				proteinfamilyWnumber.append("ProteinFamily_%s"%(proteinfamilycounter))
 				
+				proteinfamilycounter +=1
+
 				for i in proteinfamily:
 					i = i.replace("\n","")
 
 					proteinfamilyWnumber.append(i)
 				proteinfamilies.append(proteinfamilyWnumber)
 
+		proteinorthoOut.close()
 
+for i in proteinfamilies:
+	stars = 0
+	references = 0
+	referenceName =[]
+	strains = 0
+	total_indicies=0
+	for j in i:
+		if j.startswith("*"):
+			stars += 1
+			total_indicies +=1
+		elif j.startswith("ref"):
+			references += 1
+			total_indicies +=1
+			format_j= j.split("_prot")[0]
+			referenceName.append(format_j)
+
+		elif j.startswith("S"):
+			strains += 1
+			total_indicies +=1
+
+	metadata = [total_indicies,references,strains,stars,referenceName]
+
+	i = i.insert(1,metadata)
+
+#dividing proteinfamilies according to occurences in reference strains or not
+
+#making a list of references.
+
+Reflist=[]
+for i in proteinfamilies[0][2:]:
+
+	if isinstance(i,str) and not i.startswith("New"):
+		i=i.replace("_formatted.faa","")
+		i=i.replace(".faa","")
+		i=i.replace("New","")
+		Reflist.append(i)
+
+
+#lsit containing reference
+ContainingReference=[]
+for i in proteinfamilies:
+	if (i[1][1])>0:
+		ContainingReference.append(i)
+MakeProteinfamilyFiles(ContainingReference,"No_Ref",Dbfile,inputpath)
+
+#list notcontaining reference
+NotContainingReference=[]
+for i in proteinfamilies:
+	if (i[1][1])==0:
+		#print (i)
+		NotContainingReference.append(i)
+MakeProteinfamilyFiles(ContainingReference,"Ref",Dbfile,inputpath)
+
+#further dividing families:
+#creating a list of where to find the reference strains.
+
+AppendedReflist=[]
+for i in Reflist:
+	tmplist=[i]
+
+	AppendedReflist.append(tmplist)
+#print (AppendedReflist)
+
+for List in proteinfamilies:
+	for Strains in List[1][4]:
+		#print ("------------------")
+		for Reference in Reflist:
+			if Reference in Strains:
 				
-				
-proteinorthoOut.close()
+				for Refindex in AppendedReflist:
 
-Dblines=[]
-with open (Dbfile,"r") as Db:
+					if Refindex[0] ==Reference:
+						Refindex.append(List[0])
 
-	for line in Db:
-		Dblines.append(line)
-Db.close()
+# pulling the data of reference strains from the proteinfamilies list.
 
-for ProteinFamily in proteinfamilies:
-	PFlist=[]
-	for protein in ProteinFamily[1:]:
-		
-		for line in Dblines:
+for Referencelist in AppendedReflist:
+	Temporary_proteinfamily =[]
+	for Reference in Referencelist:
+		#print (Reference[1:])
+		for proteinfamily in proteinfamilies:
+			
+			if proteinfamily[0] == Reference:
+				Temporary_proteinfamily.append(proteinfamily)
 
-			if line.startswith(">"+protein):
-				print "hit" + ProteinFamily[0]
-				print protein
-				print line
-				PFlist.append(line)
-
-		with open (inputpath+"/"+ProteinFamily[0],"w")as PF:
-			for i in PFlist:
-				i=i.replace("###","\n")
-				PF.write(i)
-		PF.close()
-
-
-		
-
-
-
-
-
+	MakeProteinfamilyFiles(Temporary_proteinfamily,Referencelist[0],Dbfile,inputpath)
