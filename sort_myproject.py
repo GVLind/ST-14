8f9070118cef3bb8edb04ea81f7b35c56d3571bd
GVLind <vctrlndh@gmail.com>
2017-10-05 16:19:04 +0200

    added a outputhandler, now working for single reference strains.

--- sort_myproject.py
+++ sort_myproject.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
+########################################################################################################################
+# managing output
 
-def get_sequence(filename,filenameAllfasta):										# takes output from format_myprojectProteinortho_to_proteinfamily_dict and appends sequence from All.faa
+def flatten_FASTA_strings(filename):												# open and read fasta-files return each all ORFs as a list, 1 ORF per line.
 
-	# might have problems here due to removal of first empty element in faltten_FASTA_Strings.
-	pfamdict = format_myprojectProteinortho_to_proteinfamily_dict(filename) # get dict fom function: format_myprojectProteinortho_to_proteinfamily_dict 
+
+	with open (filename, 'r') as fasta_in:				# Opens fastafile as fasta_in
+		fastain=fasta_in.read().rstrip()				# Reads line by line		
+		fasta_out=[]									# declaring outfile
+
+		#print(fastain)					#debug
+		fastain = fastain.split(">")[1:]				#split and remove first empty list element	
+
+														# for all found ORFs
+		for line in fastain:							# looping into list of lines
+			line =line.replace("\n","\t",1).replace("\n","")				# replacing first occurece of \n with \t, to be able to remove all \n later on
+			
+														# removing all \n, leaving the element of the list as just one line containing header + sequence delimited by tab
+			fasta_out.append(line)						# appending the formatted line to the output list.
+
+		return fasta_out 							
+
+def get_sequence(pfamdict,filenameAllfasta):										# takes output from format_myprojectProteinortho_to_proteinfamily_dict and appends sequence from All.faa
+
 	
 	# manually create an All.faa in the same dir as the myproject.proteinortho
 	# $ cat *.faa > All.faa
@@ -27,29 +46,10 @@ def get_sequence(filename,filenameAllfasta):										# takes output from format
 				pfam_list_element.append("*")								# if no ORF is found appends "*
 
 		pfamcount +=1
-		print("pfams found: ",pfamcount,"/",len(pfamdict.keys()),"\r",end="")
+
+		print ("pfams found: ",pfamcount,"/",len(pfamdict.keys()),"\r",end="")
      
 	return pfamdict						
-
-def flatten_FASTA_strings(filename):												# open and read fasta-files return each all ORFs as a list, 1 ORF per line.
-
-
-
-	with open (filename, 'r') as fasta_in:				# Opens fastafile as fasta_in
-		fastain=fasta_in.read().rstrip()				# Reads line by line		
-		fasta_out=[]									# declaring outfile
-
-		print(fastain)					#debug
-		fastain = fastain.split(">")[1:]				#split and remove first empty list element	
-
-														# for all found ORFs
-		for line in fastain:							# looping into list of lines
-			line =line.replace("\n","\t",1).replace("\n","")				# replacing first occurece of \n with \t, to be able to remove all \n later on
-			
-														# removing all \n, leaving the element of the list as just one line containing header + sequence delimited by tab
-			fasta_out.append(line)						# appending the formatted line to the output list.
-
-		return fasta_out 							
 
 def format_myprojectProteinortho_to_proteinfamily_dict (filename):					# open and format proteinortho output in a dictionary, keys = proteinfamily, value = formatted proteinortho output
 
@@ -80,20 +80,77 @@ def format_myprojectProteinortho_to_proteinfamily_dict (filename):					# open an
 def make_csv_from_proteinfamily_dict(pfam_dict):									# makes a tab-delimited .csv file from dictionary with the format name=key.csv, content = value[0] \t value[2]
 
 
-	for key in pfam_dict:
-		with open(key+".csv","w") as csv_file:
+	for key in pfam_dict:												
+		filekey=key.replace(":","")										#special case for the filename
+
+		with open("%s.csv"%(filekey),"w") as csv_file:
 			for value in pfam_dict[key][3:]:							#cut away first three list element in list of each dictionary .value()
-				csv_file.write((value[0] + "\t" + value[2])+"\n")
-				
+				strainData 	= value
+				strainId	= strainData[0]
+				geneId		= strainData[1]
+
+				if len(strainData) == 3:								#if len ==3 sequence data is present along with strain id and protein id 
+					strainSeq = strainData[2]
+					csv_file.write("{0}\t{1}\t{2}\n".format(strainId,geneId,strainSeq))
+					
+
+				elif len(strainData) == 2:								# if len ==2 value is thought to contain strain id and proteinid
+					csv_file.write("{0}\t{1}\n".format(strainId,geneId))
+			print ("file %s.csv created"%(filekey))
+
+def prompt_interface():
+
+	print("\noutputmanager options: \n\n1 print to .csv files\n2 get sequences for hits\n3 print you sequences meeting criteria to the prompt\n4 print current settings to prompt\n0 exit\n")
+	userInput = input("what do you want to do? ")
+	print("\n")
+	return userInput
+
+def output_manager(pfamDictIn,pfamDictOut,settings):
+	
+
+	dictIn 	= pfamDictIn
+	dictOut = pfamDictOut
+
+	while 1:
+		userInput=prompt_interface()
+
+		if userInput=="1":
+			make_csv_from_proteinfamily_dict(dictOut)
+
+		elif userInput == "2":
+			allFasta = input("provide the name of your all.fasta:")
+
+			dictOut=get_sequence(dictOut,allFasta)
+
+			print ("Seqeunces succesfully fetched for you Ids.\n Your dictionary is now updated","blue")
+
+		elif userInput == "3":
+			import pprint
+			pp = pprint.PrettyPrinter(indent=4)
+			pp.pprint(pfamDictOut)
+		elif userInput == "4":
+			import pprint
+			pp = pprint.PrettyPrinter(indent=4)
+			pp.pprint(settings)
+		elif userInput == "0":
+			return
+		else:
+			print(" Choose one ot the above setings: ")
+
+
+#########################################################################################################################
+# managing data
+
 def get_settings (reffile):															# parse the settings or reference file.
+
 	with open (reffile,"r") as referencefile:							# opens reffile
 		
 		outlist = []													# declaring local variables
 		settinglist = []
 		settingsOut=[]
 
-		rawlist = referencefile.read().replace("\n","")					# splits lat ,
-		rawlist = rawlist.split(",")
+		rawlist = referencefile.read().replace("\n","")					#removing all newlines.
+		rawlist = rawlist.split(",")									# splits lat ,
 
 		if rawlist[len(rawlist)-1]=='':									# case that removes empty list element if edgecase with extra ,
 			rawlist=rawlist[:-1]
@@ -111,13 +168,20 @@ def get_settings (reffile):															# parse the settings or reference file
 				settinglist.append(item[1:])							# apends all settings to list.
 
 		outlist[0]=(outlist[1:len(outlist)])							# when all elements of rawlist is sorted. 0-case, is added to outlist == all outstrains.
+		#print (outlist[0]) 	#debug
 
 
 		for element in settinglist:
 			element=element.lower()										# making all lowercase
 			subelements=element.split("_")								# splittint to subelements
 
-			subelements[3] = outlist[int(subelements[3])]				# changing from number to actual outstrains, this also passes the 0-case of the outlist..
+			if subelements[3] == "all":									#handling case of all strains in outgroup
+				subelements[3] = outlist[0]
+
+			elif subelements[3].isdigit():								# handling case of single strain of output.
+				subelements[3] = outlist[int(subelements[3])]			# changing from number to actual outstrains, this also passes the 0-case of the outlist..
+			else:
+				print ("cannot find outgroup number, #  of strain or elements is needed")
 			
 			settingsOut.append(subelements)								# appending all subelements to new settingsfile.
 
@@ -218,7 +282,7 @@ def sort_pfam_evaluate_if_proteinfamily_meets_criteria (settings,pfam):				# for
 
 	for keys in pfam:
 													#for each key in dictionary the composite list is created and passed for comparison.
-		print("key %s evaluated"%(keys) )
+		#print("key %s evaluated"%(keys) ) 			#debug
 		count_in 		= 0
 		count_in_hit 	= 0
 		count_out 		= 0
@@ -272,7 +336,7 @@ def sort_pfam_evaluate_if_proteinfamily_meets_criteria (settings,pfam):				# for
 	return status_output #passes evaluation data as a list. [Key, value=True or False.]
 	
 def sort_pfam_evaluate_if_proteinfamily_meets_criteria_multiple(settings,pfam):		# placeholder for later use
-
+	print ("inside multiple")
 	
 
 	
@@ -299,9 +363,10 @@ def make_dict_of_pfam_meeting_criteria(settings,inputAllPfams,ListOfEvaluatedPfa
 			if meetsCriteria == True:
 
 				pfamsMeetingCriteria[pfam] = allPfams[pfam]
-				print(currentSetting, evaluatedPfam, meetsCriteria)				
+				#print(currentSetting, evaluatedPfam, meetsCriteria)		#debug				
 			else:
-				print(currentSetting, evaluatedPfam, meetsCriteria)
+				pass
+				#print(currentSetting, evaluatedPfam, meetsCriteria)		#debug
 
 	return pfamsMeetingCriteria
 
@@ -311,7 +376,7 @@ def make_sorted_proteinfamily_dict(settings,pfam):									# makes two cases mul
 	ListOfEvaluatedPfam =[]
 
 	for setting in settings:
-		print ("\nsetting evaluated:\n%s\n"%(setting))
+		#print ("\nsetting evaluated:\n%s\n"%(setting))				#debug
 																										#settings list[] contains a list if it's multiple inputfiles, else a string is present.
 
 		if type(setting[3])==str:																		# if comparison with 1 refstrain
@@ -336,18 +401,22 @@ def make_sorted_proteinfamily_dict(settings,pfam):									# makes two cases mul
 	# out output.csv in correct folder.
 
 
+import sys
 
+settings= get_settings(sys.argv[1])
 
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
+#print (settings)
 
-
-settings= get_settings("ref.txt")
-pfams = format_myprojectProteinortho_to_proteinfamily_dict("testmyproject.txt")
+pfams = format_myprojectProteinortho_to_proteinfamily_dict(sys.argv[2])
 pfamsMeetingCriteria = make_sorted_proteinfamily_dict(settings,pfams)
 
 
-pp.pprint(pfamsMeetingCriteria)
+
+
+
+#output_manager(pfams,pfamsMeetingCriteria,settings)
+
+
 
 """
 #test flatten_FASTA_strings
