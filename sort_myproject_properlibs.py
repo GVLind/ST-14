51046e0b29542c57a021db592202b536d46c5741
GVLind <vctrlndh@gmail.com>
2017-10-09 16:16:45 +0200

    added the last functionality for the script, can now fetch sequences and create outputfiles

--- sort_myproject_properlibs.py
+++ sort_myproject_properlibs.py
@@ -6,27 +6,40 @@ Created on Fri Oct  6 14:39:14 2017
 @author: gvl
 """
 
-
-
-def flatten_fasta_seqIO():
-    None
-def drop_unncessary_info(DataFrame):
+def drop_unncessary_info(DataFrame,columns = ['# Species', 'Genes', "Alg.-Conn."]):
     #dropping unnecessary columns.
-    columns = ['# Species', 'Genes', "Alg.-Conn."]
     DataFrame.drop(columns, inplace=True, axis=1)
     return DataFrame
 
     
 def open_proteinortho_output(myproject_fasta):    
     #open tab delimited csv file.
-    import pandas as pd
     file = myproject_fasta
-    df = pd.read_csv(file,sep="\t")
+    import pandas as pd
+    
+    if file.endswith(".csv"):
+        df = pd.read_csv(file,sep="\t",names = ["strain","gene","seq"])
+    else:
+        df = pd.read_csv(file,sep="\t")
+    
     return df
+
+def set_index_name(df):
+    # set index names as pfam + index
+    import pandas as pd
+    indexList = list(df.index)
+    newIndex=[]
+    for i in indexList:
+        newIndex.append("pfam%s"%(i+1))
+    I = pd.Index(newIndex)
+    
+    indexed_df=df.set_index(I)
+    
+    return indexed_df
 
 def set_reference():
     #set reference
-    cutOff=0.5
+    cutOff=0.95
     
     return cutOff
     
@@ -36,6 +49,7 @@ def set_in_out_group(listOut):
     return listOutGroup
     
 def convert_to_numerical(df):
+    #converts hits to numerical value 1 and misses 0
     dfN = (df!="*").astype(int)
     return dfN
 
@@ -63,42 +77,20 @@ def quota_row_full_csv(df,outGroup):
 
 
     return df
-def get_gene_names(dfPruned,dfRaw):
-    rows = list(dfPruned.index)
-    dfOut = dfRaw.iloc[rows]
-        
+def get_gene_names(df,dfRaw):
+    
+    rows = list(df.index)
+  
+    dfOut = dfRaw.loc[rows]
+
     return dfOut
     
-def setting_parser_with_json():
-    None
-    
-def data_manipulations(cutOff,listOutGroup,InputCsv,verbosity=0):
-    
-    
-    numericalInputCsv=convert_to_numerical(InputCsv)
-    statsCsv = quota_row_full_csv(numericalInputCsv,listOutGroup)
-    MeetingCriteriaCsv = drop_rows_not_meeting_criteria(statsCsv,cutOff)
-    pfamMeetingCriteria = get_gene_names(MeetingCriteriaCsv,InputCsv)
-    #get original sequence
-    
-    if verbosity ==1:
-        print("cutoff: ",cutOff)
-        print("outgroup: ",listOutGroup)
-        print("\n-----------input\n")
-        print(InputCsv)
-        print("\n-----------numerical")
-        print(numericalInputCsv)
-        print("\n-----------quotas added")
-        print(statsCsv)
-        print("\n-----------dropped rows not meeting criteria")
-        print(MeetingCriteriaCsv)
-        print("\n-----------expressed as original genes")
-        print(pfamMeetingCriteria)
-        
-    return pfamMeetingCriteria
+
+
 
 def make_list_of_int_from_string(outString,coulmns):
     
+    #makes a list of an inputstring delimited by ","
     outString=outString.split(",")
     outInt =[]
     outList = []
@@ -112,6 +104,8 @@ def make_list_of_int_from_string(outString,coulmns):
     return outList
 
 def choose_outgroup(rawInputCsv):
+    
+    # interprets user input and returns a list of a defined outgroup
     coulmns = list(rawInputCsv.columns.values)
     ok = "n"
     
@@ -135,53 +129,155 @@ def choose_outgroup(rawInputCsv):
         elif ok == "exit":
             raise SystemExit
 
-def get_ref(df,all_fasta="All.faa"):
+def choose_drops(rawInputCsv):
+    
+    # interprets user input and returns a list of strains to be dropped
+    columns = list(rawInputCsv.columns.values)
+    ok = "n"
+    
+    while 1:    
+        
+        for i in range(len(columns)):
+            print (i, columns[i])
+        outString = input("any drops? delimited with , :")
+        
+        dropList = make_list_of_int_from_string(outString,columns)
+        
+        
+        for i in dropList:
+            print (i)
+            
+        ok = input("is this correct(y/n), exit for exiting..")
+        
+        if ok == "y":
+            print ("droplist",dropList )
+            return dropList
+        
+        elif ok == "exit":
+            raise SystemExit
+    
+def drop_pfam(df,dropList):
+    #drops columns from df based on input list
+    print("inside drop_pfam")
+    print(dropList)
+    df.drop(dropList, axis=1, inplace=True)
+    return df
+
+
+
+def print_out(df,fileName="output"):
+    
+    # prints df with fileName to .csv handles earlies .csv separately to get correct naming
+    if fileName.endswith(".csv"):
+        fileName=("Seq_"+fileName)
+    else:
+        fileName +=".csv" 
+    print ("printing to %s.."%(fileName))
+    df.to_csv(fileName, encoding='utf-8',sep="\t")
+    return fileName
+
+
+def get_seq(df,allfasta="All.faa"):
+    
+    #gets sequence from All.fasta, based on the column of genes, handles misses and hits, not multiple hits.
     from Bio import SeqIO
-    fasta_dict = SeqIO.index(all_fasta, "fasta")
+    record_dict = SeqIO.to_dict(SeqIO.parse(allfasta, "fasta"))
     
-    indexList = list(df.index)
-    for i in indexList:
-        print (fasta_dict[i])
+    dfGenes = df["gene"].tolist()
+    seqs=[]
+    for i in dfGenes:
+        
+        if i != "*":
+            rec=record_dict[i]
+            print(rec.name,"\t",rec.seq)
+            seqs.append(str(rec.seq))
+        else:
+            seqs.append("")
     
-    #for i in indexList:
-     #   print fasta_dict[i]
-    
-    
-def inputmanager():
-    
-    rawInputCsv = open_proteinortho_output('testmyproject.txt')
-    inputCsv = drop_unncessary_info(rawInputCsv)
-    listOutGroup = choose_outgroup(inputCsv)#["PittEE","KR494"]     #
-    cutOff = set_reference()
-    
-    
-    df=data_manipulations(cutOff,listOutGroup,rawInputCsv,verbosity = 0)
+    df["seq"]=seqs
     
     return df
 
-def print_out(df,fileName="output.csv"):
-    print ("printing to %s.."%(fileName))
-    df.to_csv(fileName, encoding='utf-8')
 
-    
-def outputmanager(df):
-    
-    listOfColumns=list(df.index)
-    for i in listOfColumns:
+def format_df(df):
+    #Transpose and split df based on pfam, returns multiple dfs.
+  
+    formattedFrames =[]
+    ind = list(df.index)
+
+    df = df.T
+
+    for i in ind:
         
-        print(i,"\n",df.loc[i])
-        get_ref(df.loc[i])
-        #print_out(df.loc[i],"%s.csv"%(i))
-    
-    print ("\n-----------output")
+        dfC=df[i]
+        formattedFrames.append(dfC)
+    return formattedFrames
 
+def data_manipulations(cutOff,listOutGroup,InputCsv,verbosity=0):
+    
+    #convert to numerical
+    numericalInputCsv=convert_to_numerical(InputCsv)
+    
+    #get stats of each row ie pfam
+    statsCsv = quota_row_full_csv(numericalInputCsv,listOutGroup)
+    
+    # evaluates if meeting criteria drop other rowa
+    MeetingCriteriaCsv = drop_rows_not_meeting_criteria(statsCsv,cutOff)
+    
+    # using original names
+    pfamMeetingCriteria = get_gene_names(MeetingCriteriaCsv,InputCsv)
+
+    # output verbosity    
+    if verbosity ==1:
+        print("cutoff: ",cutOff)
+        print("outgroup: ",listOutGroup)
+        print("\n-----------input\n")
+        print(InputCsv)
+        print("\n-----------numerical")
+        print(numericalInputCsv)
+        print("\n-----------quotas added")
+        print(statsCsv)
+        print("\n-----------dropped rows not meeting criteria")
+        print(MeetingCriteriaCsv)
+        print("\n-----------expressed as original genes")
+        print(pfamMeetingCriteria)
+        
+    return pfamMeetingCriteria
+
+def inputmanager():
+    # gluing together functions that manages the iunput
+    rawInputCsv = open_proteinortho_output('myproject.proteinortho')
+    
+    inputCsv = drop_unncessary_info(rawInputCsv)
+    
+    indexedCsv = set_index_name(inputCsv)
+    
+    prunedCsv = drop_pfam(indexedCsv,choose_drops(indexedCsv))
+    
+    listOutGroup = choose_outgroup(prunedCsv) #     ["PittEE","KR494"]
+        
+    cutOff = set_reference()
+    
+    df=data_manipulations(cutOff,listOutGroup,indexedCsv,verbosity = 0)
+    
+    return df
+
+def outputmanager(df):
+    #handles output functions.
+    ans = input("grap sequences? (y?) : " )    
+    listFormattedDf= format_df(df)
+    
+    
+    for i in listFormattedDf:
+        fileName = print_out(i,i.name)
+        df = open_proteinortho_output(fileName)
+        if ans == "y":
+            dfSeq = get_seq(df)
+            print_out(dfSeq,fileName)
+    
     return
     
 df = inputmanager()
 outputmanager(df)
 
 
-
-#print(df)
-#write and outputmanager that gets sequences and prints to csv.
-#outputmanager(df)
